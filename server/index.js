5af46925651722d62006bca60685d07c7035ea71
Shivkumarrathod <137758681+Shivkumarrathod@users.noreply.github.com>
2024-06-07 21:28:19 +0530

    deleting

--- server/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const app = express()
-const PORT=process.env.PORT||5000
-import recordingRouter from './Router/recording.js'
-
-app.use('/api/record',recordingRouter)
-app.listen(PORT,()=>console.log(`Server is Running AT ${PORT}`))
