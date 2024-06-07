7b9818091e0d81a25245ba03776c6ae4caaf0aad
Shivkumarrathod <137758681+Shivkumarrathod@users.noreply.github.com>
2024-06-07 10:59:03 +0530

    tailwindcss and react project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-pink-500 p-2 text-center'>hello</h1>
+    </>
+  )
+}
+
+export default App
